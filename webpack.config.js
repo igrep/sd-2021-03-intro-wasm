bc9a549e4d42b7bfeaf4ca8e99fe4e4e6a20dfb7
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2021-05-12 22:06:26 +0900

    Update dependencies to avoid the vulnerability

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,6 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  mode: "development"
+  mode: "development",
+  experiments: { asyncWebAssembly: true },
 };
